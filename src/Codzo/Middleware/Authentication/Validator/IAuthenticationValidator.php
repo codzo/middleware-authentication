5595fa5a0f359fbb8fbc5df496c73e43360f7f0e
neilfan <neil.fan@codzo.com>
2020-09-26 10:13:03 +1000

    return bool for isAuthenticated()

--- src/Codzo/Middleware/Authentication/Validator/IAuthenticationValidator.php
+++ src/Codzo/Middleware/Authentication/Validator/IAuthenticationValidator.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 interface IAuthenticationValidator
 {
     public function __construct(Request $request=null);
-    public function isAuthenticated() : self  ;
+    public function isAuthenticated() : bool  ;
     public function authenticate() : self ;
     public function revoke() : self ;
 }
