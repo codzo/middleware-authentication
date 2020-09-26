b7b456a26977a2abd3ca3ab90668676ad3717eb3
neilfan <neil.fan@codzo.com>
2020-09-26 10:03:34 +1000

    update Session Validator to spec of IAuthenticationValidator

--- src/Codzo/Middleware/Authentication/Validator/SessionValidator.php
+++ src/Codzo/Middleware/Authentication/Validator/SessionValidator.php
@@ -12,20 +12,20 @@ class SessionValidator implements IAuthenticationValidator
     {
     }
 
-    public function isAuthenticated(Request $request=null) : bool
+    public function isAuthenticated() : bool
     {
         return key_exists($_SESSION[self::SESSION_VARNAME])
             && $_SESSION[self::SESSION_VARNAME];
     }
 
-    public function authenticate(Request $request=null) : self
+    public function authenticate() : self
     {
         $_SESSION[self::SESSION_VARNAME] = true;
 
         return $this;
     }
 
-    public function revoke(Request $request=null) : self
+    public function revoke() : self
     {
         unset($_SESSION[self::SESSION_VARNAME]);
 
